f2dfa41d810a8fd08319e356f1407fa3b3f0b073
Souravroy99 <souravroy200418@gmail.com>
2025-11-11 22:15:26 +0530

    Working on Controller

---
+++ Controllers/TaskController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NotificationService.Data;
+
+namespace NotificationCenter.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public TaskController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+    }
+}
